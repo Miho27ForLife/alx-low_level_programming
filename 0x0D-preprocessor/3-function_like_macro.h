2a029a9deef486e098e988df06b5b5c33c12db99
Mohamed BOUICHOU <mohamed.bouichou1@gmail.com>
2023-07-17 10:03:57 -0700

    again

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) (((x) < (0)) ? -((x) * (1)) : (x))
+#define ABS(x) ((x) < (0) ? (-x) : (x))
 
 #endif
-
-//(((x) < (0)) ? -((x) * (1)) : (x))
